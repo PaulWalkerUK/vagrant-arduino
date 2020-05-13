1f076734fedf3b872e43f05b77b44a4900da7469
PaulWalkerUK <walker.paul+github@gmail.com>
2020-05-13 23:24:12 +0100

    Setup Arduino IDE, serial port connection + tools

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -10,3 +10,20 @@ include vbox_guest_additions
 include xorg
 include lightdm
 include xfce
+
+package { 'ttf-dejavu': ensure => 'present' }
+package { 'mousepad': ensure => 'present' } # text editor
+package { 'vim': ensure => 'present' } # text editor
+
+package { 'arduino': ensure => 'present' }
+
+exec { 'user_permissions_on_serial_port':
+  command => '/usr/bin/usermod -a -G uucp vagrant'
+}
+
+package { 'firefox': ensure => 'present' }
+package { 'git': ensure => 'present' }
+package { 'thunar-archive-plugin': ensure => 'present' }
+package { 'xarchiver': ensure => 'present' }
+package { 'unzip': ensure => 'present' }
+package { 'zip': ensure => 'present' }
